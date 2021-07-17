ea84b0293decb0466b57597b7b4f363ea239a548
Jesus24DarkSaider <76741670+Jesus24DarkSaider@users.noreply.github.com>
2021-07-17 10:25:44 -0500

    Create condicionales en dart.dart

---
+++ condicionales en dart.dart
@@ -0,0 +1,16 @@
+void main(){
+  
+  String empleado;
+  const bool isAplicaSeguro = false;
+  
+  empleado = 'Jesus Vallejo Alvarado';
+  
+  if(isAplicaSeguro == true)
+  {
+    print('El empleado: $empleado aplica seguro');
+  }
+  else
+  {
+    print('No aplica seguro');
+  }
+}
