f159384519f1177ddab3d1206fb7183eb95e9395
Jesus24DarkSaider <76741670+Jesus24DarkSaider@users.noreply.github.com>
2021-07-17 11:29:31 -0500

    Create mapsTipados.dart

---
+++ mapsTipados.dart
@@ -0,0 +1,12 @@
+void main(){
+ 
+  Map<String,dynamic> persona = 
+  {
+    'nombre':'Jesus Vallejo',
+    'Edad':23,
+    'isActivo': true,
+  };
+  print(persona['nombre']);
+  print(persona['Edad']);
+  print(persona['isActivo']);
+}
