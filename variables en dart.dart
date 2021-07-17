874fa4e97bf4e56cbf80cf7fdcb10d0fc3f6e4b0
Jesus24DarkSaider <76741670+Jesus24DarkSaider@users.noreply.github.com>
2021-07-17 10:12:28 -0500

    Create variables en dart.dart

---
+++ variables en dart.dart
@@ -0,0 +1,29 @@
+void main (){
+  
+  /*
+   * Tipo de Dato - USO
+   * String - Cadena de caracteres 
+   * int -  Numeros enteros
+   * double - Numeros decimales
+   * boolean - Valores unicos true o false
+   **/
+   
+   String nombre;
+   String apellido;
+   int edad;
+   double sueldo;
+   bool isAplicaSeguro;  
+  
+   nombre = "Carlos de Jesus";
+   apellido ="Vallejo Alvarado";
+   edad = 23;
+   sueldo = 500.50;
+   isAplicaSeguro = true;
+  
+   print('---- EMPLEADO ----');
+   print('Nombre: $nombre');
+   print('Apellido: $apellido');
+   print('Edad: $edad');
+   print('Sueldo: $sueldo');
+   print('Aplica seguro: $isAplicaSeguro');
+}
